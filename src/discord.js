839dc145728a63b802d9e8d9bc60172ce4c65bb2
ChatDisabled <44729807+ChatDisabled@users.noreply.github.com>
2023-12-06 15:33:36 +0100

    chore: testing a very long commit message by me

--- src/discord.js
+++ src/discord.js
@@ -1,5 +1,5 @@
 const { MessageEmbed, WebhookClient } = require('discord.js')
-const MAX_MESSAGE_LENGTH = 40
+const MAX_MESSAGE_LENGTH = 72
 
 module.exports.send = (id, token, repo, branch, url, commits, size, threadId) =>
   new Promise((resolve, reject) => {
